6e8272d16bd1605f242b8328608e12c2888641cc
Amanda Xavier Mariano <42248042+amandaxm@users.noreply.github.com>
2020-06-01 15:29:51 -0300

    Consulta simples @query

--- src/main/java/com/workshop/course/repository/PostRepository.java
+++ src/main/java/com/workshop/course/repository/PostRepository.java
@@ -3,21 +3,19 @@ package com.workshop.course.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.workshop.course.domain.Post;
 
-
-
-
-
 @Repository
-
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
-
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+
+	List<Post> searchTitle(String text);
 
 }
--- src/main/java/com/workshop/course/services/PostService.java
+++ src/main/java/com/workshop/course/services/PostService.java
@@ -34,8 +34,7 @@ public class PostService {
 
 	public List<Post> findByTitle(String text) {
 
-		return repo.findByTitleContainingIgnoreCase(text);
-
+		return repo.searchTitle(text);
 	}
 
 }
