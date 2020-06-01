1b10f480c4f2e9bb6090e2f7a73b04d765d9217a
Amanda Xavier Mariano <42248042+amandaxm@users.noreply.github.com>
2020-06-01 15:55:12 -0300

    Consulta com vários critérios

--- src/main/java/com/workshop/course/resource/util/URL.java
+++ src/main/java/com/workshop/course/resource/util/URL.java
@@ -2,6 +2,9 @@ package com.workshop.course.resource.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -15,6 +18,23 @@ public class URL {
 
 			return "";
 
+		}
+
+	}
+
+	public static Date convertDate(String textDate, Date defaultValue) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		try {
+			return sdf.parse(textDate);
+
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			return defaultValue;
 		}
 
 	}
